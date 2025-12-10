db0396cb1694f996aea8337a0b1ff5a853ecaea3
Kataleko <katalekoweb@gmail.com>
2025-12-10 09:56:50 +0100

    install junit

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Calc {
+
+    public int divide (int n1, int n2) {
+        return n1/n2;
+    }
+
+    public int mutiply (int n1, int n2) {
+        return n1*n2;
+    }
+
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalcTest {
+    @Test
+    public void test () {
+        System.out.println("First Junit run");
+    }
+}
